1bd2e7d76320f4da4ff4b577e2dfec3a8f3e618b
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2025-04-06 19:51:16 +0500

    Add SQL to project, and test code for it. Also upgraded .gitignore and deleted .pro.user (#2)
    * Revise .gitignore for more files, and delete now ignored .pro.user file
    
    * Update .gitignore to include build
    
    * Add SQL to project, and add test code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <iostream>
+#include <QSqlDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QStringList drivers = QSqlDatabase::drivers();
+    std::cout << "Available Qt SQL Drivers:\n";
+    for (const QString &driver : drivers) {
+        std::cout << driver.toStdString() << std::endl;
+    }
+
+
     MainWindow w;
     w.show();
+
     return a.exec();
 }
